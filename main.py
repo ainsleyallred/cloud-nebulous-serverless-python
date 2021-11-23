b0530156a427a240bd2bf58b02301710f1c4febb
ainsleyallred <70336591+ainsleyallred@users.noreply.github.com>
2021-11-23 11:29:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ app = Flask(__name__)
 @app.route('/', methods =["GET", "POST"])
 def gfg():
 	if request.method == "POST":
-	# getting input with name = fname in HTML form
-	first_name = request.form.get("fname")
-	# getting input with name = lname in HTML form
-	last_name = request.form.get("lname")
-	return "Your name is "+first_name + last_name
+		# getting input with name = fname in HTML form
+		first_name = request.form.get("fname")
+		# getting input with name = lname in HTML form
+		last_name = request.form.get("lname")
+		return "Your name is "+first_name + last_name
 	return render_template("form.html")
 
 if __name__=='__main__':
